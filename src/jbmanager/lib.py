2c6390e2c2d6a87c6682371726605930e7462a9d
eyllanesc <e.yllanescucho@gmail.com>
2023-12-28 20:07:20 -0500

    update

---
+++ src/jbmanager/lib.py
@@ -0,0 +1,42 @@
+import yaml
+from pydantic import BaseModel
+import openpyxl
+from pathlib import Path
+from typing import Any
+import shutil
+
+
+class Config(BaseModel):
+    institute_name: str
+    enrollment: int
+    monthly_payment: int
+    template_directory: Path
+    output_directory: Path
+
+
+def load_config(filename: str) -> Any:
+    with open(filename, "r") as file:
+        return yaml.safe_load(file)
+
+
+def validate_config(filename: str) -> Config | None:
+    data = load_config(filename)
+    try:
+        return Config(**data)
+    except Exception as e:
+        print(e)
+        return
+
+
+def copy_template(
+    in_directory: Path,
+    out_directory: Path,
+    config: Config,
+    delete_if_exist: bool = True,
+) -> None:
+    if delete_if_exist:
+        shutil.rmtree(out_directory)
+    shutil.copytree(in_directory, out_directory, dirs_exist_ok=True)
+    institute_file = out_directory / "INSTITUTO"
+    institute_file.rename(out_directory / config.institute_name)
+    print(institute_file)
